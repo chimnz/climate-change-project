42ff29e730e1b58d6cc9dac3c09310d7de145ece
Christian Chimezie <chris.chimezie@gmail.com>
2020-04-18 12:36:27 -0400

    Add function to load soi data.

--- scripts/tools.py
+++ scripts/tools.py
@@ -29,8 +29,27 @@ class DataLoader(object):
 		a = np.array([year, month, temp])
 		return np.transpose(a)
 
+	def __linearize_month_data_cols(self, yearcol, monthcols):
+		'''Linerarize data stored in month columns (global and soi).'''
+		temp = []
+		monthindex = 0
+		yearindex = 0
+		years_processed = 0
+		while years_processed < len(yearcol):
+			t = monthcols[monthindex][yearindex]
+			temp.append(t)
+
+			monthindex += 1
+			if monthindex == 12:
+				monthindex = 0  # return to first columns
+				yearindex += 1  # move to next row
+				years_processed += 1
+		# [jan1980, feb1980, ..., dec2019]
+		return temp
+
+
 	def global_temp(self):
-		year, month, temp = [], [], []
+		year, month = [], []
 		rows = np.loadtxt(
 			fname=self.global_temp_filepath,
 			delimiter=',',
@@ -40,43 +59,48 @@ class DataLoader(object):
 
 		# 1980, 1981, 1982, ...
 		yearcol = rows[0]
-		# [ [jan1980, jan1981, ...], [feb1980, feb1981, ...], ... ]
+		# temp: [ [jan1980, jan1981, ...], [feb1980, feb1981, ...],
+		# ..., [dec1980, dec1981] ]
 		monthcols = rows[1:13]
 		
-		# collect years
 		for y in yearcol:
 			year += [y for i in range(12)]
-		# collect months
 		for y in yearcol:
 			for n in range(1, 13):
 				month.append(n)
-		# collect temps
-		num_iter = 0
-		monthindex = 0
-		yearindex = 0
-		while num_iter < len(year):
-			t = monthcols[monthindex][yearindex]
-			temp.append(t)
-
-			monthindex += 1
-			if monthindex == 12:
-				monthindex = 0  # return to first columns
-				yearindex += 1  # move to next row
-			
-			num_iter += 1
+		temp = self.__linearize_month_data_cols(yearcol, monthcols)
 
 		# year, month, temp
 		a = np.array([year, month, temp])
 		return np.transpose(a)
 
-	def data(self):
-		local_temp = self.local_temp()
-		global_temp = self.global_temp()
+	def soi(self):
+		# same format as global temp
+		year, month = [], []
+		rows = np.loadtxt(
+			fname=self.soi_filepath,
+			unpack=True,
+			skiprows=1,
+		)
 
+		yearcol = rows[0]
+		monthcols = rows[1:]
+
+		for y in yearcol:
+			year += [y for i in range(12)] 
+		for y in yearcol:
+			for n in range(1, 13):
+				month.append(n)
+		temp = self.__linearize_month_data_cols(yearcol, monthcols)
+
+		# year, month, temp
+		a = np.array([year, month, temp])
+		return np.transpose(a)
+
+
+	def load(self):
 		return {
-			'local': local_temp,
-			'global': global_temp,
+			'local': self.local_temp(),
+			'global': self.global_temp(),
+			'soi': self.soi()
 		}
-
-dl = DataLoader()
-print( dl.data() )
