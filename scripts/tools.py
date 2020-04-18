ae47e99a73552ce9a046502081121d971989b816
Christian Chimezie <chris.chimezie@gmail.com>
2020-04-18 11:40:47 -0400

    Add script to load global temp data.

--- scripts/tools.py
+++ scripts/tools.py
@@ -29,10 +29,54 @@ class DataLoader(object):
 		a = np.array([year, month, temp])
 		return np.transpose(a)
 
+	def global_temp(self):
+		year, month, temp = [], [], []
+		rows = np.loadtxt(
+			fname=self.global_temp_filepath,
+			delimiter=',',
+			unpack=True,
+			skiprows=2,
+		)
+
+		# 1980, 1981, 1982, ...
+		yearcol = rows[0]
+		# [ [jan1980, jan1981, ...], [feb1980, feb1981, ...], ... ]
+		monthcols = rows[1:13]
+		
+		# collect years
+		for y in yearcol:
+			year += [y for i in range(12)]
+		# collect months
+		for y in yearcol:
+			for n in range(1, 13):
+				month.append(n)
+		# collect temps
+		num_iter = 0
+		monthindex = 0
+		yearindex = 0
+		while num_iter < len(year):
+			t = monthcols[monthindex][yearindex]
+			temp.append(t)
+
+			monthindex += 1
+			if monthindex == 12:
+				monthindex = 0  # return to first columns
+				yearindex += 1  # move to next row
+			
+			num_iter += 1
+
+		# year, month, temp
+		a = np.array([year, month, temp])
+		return np.transpose(a)
+
 	def data(self):
 		local_temp = self.local_temp()
+		global_temp = self.global_temp()
 
-		return local_temp
+		return {
+			'local': local_temp,
+			'global': global_temp,
+		}
 
 dl = DataLoader()
 print( dl.data() )
