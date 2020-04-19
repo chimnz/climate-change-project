0347e342e2e52923e25164fa575236187e34063b
Christian Chimezie <chris.chimezie@gmail.com>
2020-04-19 14:54:24 -0400

    Update plotting script to output global time series fit.

--- scripts/makeplots.py
+++ scripts/makeplots.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from tools import DataLoader
 
 dl = DataLoader()
-data = dl.load()
+data = dl.load(unpack=True)
 
 localdata = data['local']
 globaldata = data['global']
@@ -13,15 +13,16 @@ soidata =  data['soi']
 plt.figure(figsize=(10,5))
 
 # fits
+# ---------------------------------------------------------
 ## local vs. time
 ### scatter data points
-year, month, temp = soidata
-t = np.array(list(set(year)))  # years since January, 1980
+year, month, temp = localdata
+t = np.array(list(set(year)))
 temp = np.mean(temp.reshape(-1, 12), axis=1)  # year-wise average
 plt.scatter(t, temp, marker='.', c='black')
 ### label axes and title graph
-plt.title('Local Temperature Trend')
-plt.xlabel('Years since January 1980')
+plt.title('Local Temperature Trend Over Last 40 Years')
+plt.xlabel('Year')
 plt.ylabel('Temperature ($^{\circ}$ C)')
 ### plot best fit line
 m, b = np.polyfit(t, temp, deg=1)
@@ -30,5 +31,22 @@ plt.plot(t, m*t+b, c='red')
 plt.savefig('../plots/fits/local.png')
 ### clear figure
 plt.clf()
-
+# ---------------------------------------------------------
 ## global vs. time
+### scatter data points
+year, month, temp = globaldata
+t = np.array(list(set(year)))
+temp = np.mean(temp.reshape(-1, 12), axis=1)  # year-wise average
+plt.scatter(t, temp, marker='.', c='black')
+### label axes and title graph
+plt.title('Global Temperature Trend Over Last 40 Years')
+plt.xlabel('Year')
+plt.ylabel('Temperature Anomaly ($^{\circ}$ C)')
+### plot best fit line
+m, b = np.polyfit(t, temp, deg=1)
+plt.plot(t, m*t+b, c='green')
+### output plot to png
+plt.savefig('../plots/fits/global.ps')
+### clear figure
+plt.clf()
+# ---------------------------------------------------------
