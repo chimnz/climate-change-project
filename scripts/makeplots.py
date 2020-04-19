26a8f1715399e998da95a028bab2d3b47e48d167
Christian Chimezie <chris.chimezie@gmail.com>
2020-04-19 15:05:11 -0400

    Update plotting script to output soi time series fit.

--- scripts/makeplots.py
+++ scripts/makeplots.py
@@ -17,7 +17,7 @@ plt.figure(figsize=(10,5))
 ## local vs. time
 ### scatter data points
 year, month, temp = localdata
-t = np.array(list(set(year)))
+t = np.array(list(set(year)))  # 1980, 1981, ..., 2019
 temp = np.mean(temp.reshape(-1, 12), axis=1)  # year-wise average
 plt.scatter(t, temp, marker='.', c='black')
 ### label axes and title graph
@@ -33,20 +33,25 @@ plt.savefig('../plots/fits/local.png')
 plt.clf()
 # ---------------------------------------------------------
 ## global vs. time
-### scatter data points
-year, month, temp = globaldata
-t = np.array(list(set(year)))
-temp = np.mean(temp.reshape(-1, 12), axis=1)  # year-wise average
+temp = globaldata[2]
+temp = np.mean(temp.reshape(-1, 12), axis=1)
 plt.scatter(t, temp, marker='.', c='black')
-### label axes and title graph
 plt.title('Global Temperature Trend Over Last 40 Years')
 plt.xlabel('Year')
 plt.ylabel('Temperature Anomaly ($^{\circ}$ C)')
-### plot best fit line
 m, b = np.polyfit(t, temp, deg=1)
 plt.plot(t, m*t+b, c='green')
-### output plot to png
-plt.savefig('../plots/fits/global.ps')
-### clear figure
+plt.savefig('../plots/fits/global.png')
 plt.clf()
 # ---------------------------------------------------------
+## soi vs. time
+soi = soidata[2]
+soi = np.mean(soi.reshape(-1, 12), axis=1)
+plt.scatter(t, soi, marker='.', c='black')
+plt.title('Southern Oscillation Index Trend Over Last 40 Years')
+plt.ylabel('SOI')
+plt.xlabel('Year')
+m, b = np.polyfit(t, soi, deg=1)
+plt.plot(t, m*t+b, c='purple')
+plt.savefig('../plots/fits/soi.png')
+plt.clf()
